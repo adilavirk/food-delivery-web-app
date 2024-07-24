111586d3fe58002ebe46b8e956e1e4d355c2ed16
Adila Arshad <143010220+adilavirk@users.noreply.github.com>
2024-07-24 12:18:38 +0500

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -52,8 +52,10 @@ const placeOrder = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       line_items: line_items,
       mode: "payment",
-      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+      success_url: `https://food-delivery-web-app-frontend.vercel.app/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `https://food-delivery-web-app-frontend.vercel.app/verify?success=false&orderId=${newOrder._id}`,
+      // success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+      // cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
     });
 
     console.log("Success URL:", session.success_url); // Log the success URL
